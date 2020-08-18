b6c121246c1e3cd956f418f10736780cb20ef86f
hitskyer <xxjroom@163.com>
2020-08-18 16:07:23 +0800

    增加bert+rnn代码

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import utils
 import train
 
 parser = argparse.ArgumentParser(description='Skyer-Bert-Text-Classsification')
-parser.add_argument('--model', type=str, default='SkyerBertCNN', help = 'choose a model SkyerBert, SkyerBertCNN')
+parser.add_argument('--model', type=str, default='SkyerBertRNN',
+                    help = 'choose a model SkyerBert, SkyerBertCNN, SkyerBertRNN')
 args = parser.parse_args()
 
 if __name__ == '__main__':
